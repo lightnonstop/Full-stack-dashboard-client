541e64379363ea393d4e26d3320cf1033db5c1b1
lightnonstop <umichaeledet003@gmail.com>
2023-06-26 20:35:31 +0100

    split app store and hook into modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material'
+import { useAppSelector } from 'app/hook'
+import { themeSettings } from 'theme'
 function App() {
+  const mode = useAppSelector((state) => state.global.mode)
   return (
     <>
       
