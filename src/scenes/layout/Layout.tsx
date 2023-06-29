eaeb3b218211575c48ebd4a00947c8f165aea46b
lightnonstop <umichaeledet003@gmail.com>
2023-06-29 13:23:40 +0100

    item take full space of flex container

--- src/scenes/layout/Layout.tsx
+++ src/scenes/layout/Layout.tsx
@@ -9,9 +9,9 @@ function Layout() {
   const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true)
   return (
     <Box display={isNonMobile ? 'flex' : 'block'} width='100%' height='100%'>
-      <Sidebar isNonMobile={isNonMobile} isSidebarOpen={isSidebarOpen} drawerWidth='250px' setIsSidebarOpen={setIsSidebarOpen} />
-      <Box>
-        <Navbar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
+      <Sidebar user={data || {}} isNonMobile={isNonMobile} isSidebarOpen={isSidebarOpen} drawerWidth='250px' setIsSidebarOpen={setIsSidebarOpen} />
+      <Box flexGrow={1}>
+        <Navbar user={data || {}} isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
         <Outlet />
       </Box>
     </Box>
