0df8c8fe72bd1e909e3c4a1ba179ff146d37979f
lightnonstop <umichaeledet003@gmail.com>
2023-06-29 12:55:09 +0100

    setup app listeners

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,15 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
 import globalReducer from '../state/state'
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import { api } from '../state/api';
 export const store = configureStore({
     reducer: {
       global: globalReducer,
+      [api.reducerPath]: api.reducer,
     },
-})
+    middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
