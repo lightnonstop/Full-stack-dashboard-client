e328360bf8f9651167fa69f5cc245651b02e1efc
lightnonstop <umichaeledet003@gmail.com>
2023-07-01 02:28:17 +0100

    add 'standard' variant

---
+++ src/components/DataGridCustomToolbar.tsx
@@ -0,0 +1,41 @@
+import { GridToolbarColumnsButton, GridToolbarContainer, GridToolbarDensitySelector, GridToolbarExport } from "@mui/x-data-grid";
+import FlexBetween from "./FlexBetween";
+import { IconButton, InputAdornment, TextField } from "@mui/material";
+import { Search } from "@mui/icons-material";
+
+function DataGridCustomToolbar({ searchInput, setSearchInput, setSearch }) {
+    return (
+        <GridToolbarContainer>
+            <FlexBetween width='100%'>
+                <FlexBetween>
+                    <GridToolbarColumnsButton />
+                    <GridToolbarDensitySelector />
+                    <GridToolbarExport />
+                </FlexBetween>
+                <TextField
+                    label='Search...'
+                    sx={{ mb: '0.5rem', width: '15rem' }}
+                    onChange={(e) => setSearchInput(e.target.value)}
+                    value={searchInput}
+                    variant='standard'
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton
+                                onClick={() => {
+                                    setSearch(searchInput)
+                                    setSearchInput('')
+                                }}
+                                >
+                                    <Search />
+                                </IconButton>
+                            </InputAdornment>
+                        )
+                    }}
+                />
+            </FlexBetween>
+        </GridToolbarContainer>
+    )
+}
+
+export default DataGridCustomToolbar
