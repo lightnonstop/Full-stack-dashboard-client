da6d3a93f8e12fad1d7613784aab33f896d3b39e
lightnonstop <umichaeledet003@gmail.com>
2023-07-03 11:25:39 +0100

    add common header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { useTheme } from '@emotion/react'
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+interface HeaderProps{
+    title: string
+    subtitle: string
+}
+function Header({ title, subtitle }: HeaderProps) {
+    const theme = useTheme()
+  return (
+    <Box>
+        <Typography  variant='h2' color={theme.palette.secondary[100]}  fontWeight='bold' sx={{ mb: '5px' }}>
+            {title}
+        </Typography>
+        <Typography variant='h5' color={theme.palette.secondary[300]}>
+            {subtitle}
+        </Typography>
+    </Box>
+  )
+}
+
+export default Header
