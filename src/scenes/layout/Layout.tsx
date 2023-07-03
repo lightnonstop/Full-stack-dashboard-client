12e195a08e89de337486a22f2e609639ca2c12c7
lightnonstop <umichaeledet003@gmail.com>
2023-07-03 11:29:17 +0100

    remove log statement, add userId

--- src/scenes/layout/Layout.tsx
+++ src/scenes/layout/Layout.tsx
@@ -3,10 +3,15 @@ import Navbar from '../../components/Navbar'
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../../components/Sidebar';
 import { useState } from 'react';
+import { useAppSelector } from '../../app/hook';
+import { useGetUserQuery } from '../../state/api';
 
 function Layout() {
   const isNonMobile =  useMediaQuery('(min-width: 600px');
   const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true)
+  const userId = useAppSelector(state => state.global.userId)
+  const { data } = useGetUserQuery(userId);
+
   return (
     <Box display={isNonMobile ? 'flex' : 'block'} width='100%' height='100%'>
       <Sidebar user={data || {}} isNonMobile={isNonMobile} isSidebarOpen={isSidebarOpen} drawerWidth='250px' setIsSidebarOpen={setIsSidebarOpen} />
