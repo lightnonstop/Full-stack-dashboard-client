3f6a1d0f028d96267820a3b3dfb65d4fdb900ffd
lightnonstop <umichaeledet003@gmail.com>
2023-07-05 13:13:12 +0100

    add dashboard query endpoint and minor names fixes

--- src/state/api.ts
+++ src/state/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_APP_BASE_URL }),
     reducerPath: 'adminApi',
-    tagTypes: ['User', 'Products', 'Customers', 'Transactions', 'Geography', 'Sales'],
+    tagTypes: ['User', 'Products', 'Customers', 'Transactions', 'Geography', 'Sales', 'Admins', 'Performance', 'Dashboard'],
     endpoints: (build) => ({
         getUser: build.query({
             query: (id) => `general/user/${id}`,
@@ -37,6 +37,14 @@ export const api = createApi({
             query: () => 'management/admins',
             providesTags: ['Admins']
         }),
+        getPerformance: build.query({
+            query: (id) => `management/performance/${id}`,
+            providesTags: ['Performance']
+        }),
+        getDashboard: build.query({
+            query: () => `general/dashboard/${id}`,
+            providesTags: ['Dashboard']
+        }),
     })
 })
 
@@ -47,4 +55,7 @@ export const {
     useGetTransactionsQuery,
     useGetGeographyQuery,
     useGetSalesQuery,
+    useGetAdminsQuery,
+    useGetPerformanceQuery,
+    useGetDashboardQuery,
 } = api;
